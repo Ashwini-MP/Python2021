469bffb0fc9e3bfbfa117e01fede4d14a8420552
Ashwini-MP <ashwini.prakash1@gmail.com>
2021-09-20 10:34:58 -0400

    updated file

--- elevator.py
+++ elevator.py
@@ -1,19 +1,23 @@
 while True:
     print("Welcome to the elevator")
     floor_select = int(input("Please select floor 1-10:  "))
-    current_floor = int(input("Enter the current floor: "))
-    if floor_select==current_floor:
-        print("Your have arrived {} floor". format(floor_select))
-    while floor_select > current_floor:
-        print("Elevator is going up and you are currently on the floor number {}".format(current_floor))
-        current_floor += 1
-        if floor_select == current_floor:
-            print("Your have arrived the floor number {}".format(floor_select))
-            break
-    while floor_select < current_floor:
-        print("Elevator is going down and you are currently on the floor number {}".format(current_floor))
-        current_floor -= 1
-        if floor_select == current_floor:
-            print("Your have arrived  the floor number {}".format(floor_select))
-            break
+    if floor_select > 10 or floor_select < 1:
+        print("Invalid entry, Try again")
+    else:
+        current_floor = int(input("Enter the current floor: "))
+        if floor_select==current_floor:
+            print("Your have arrived the floor {}". format(floor_select))
+
+        while current_floor < floor_select:
+            print("Elevator is going up and you are currently on the floor number {}".format(current_floor))
+            current_floor += 1
+            if floor_select == current_floor:
+                print("Your have arrived the floor number {}".format(floor_select))
+                break
+        while current_floor > floor_select:
+            print("Elevator is going down and you are currently on the floor number {}".format(current_floor))
+            current_floor -= 1
+            if floor_select == current_floor:
+                print("Your have arrived  the floor number {}".format(floor_select))
+                break
 
